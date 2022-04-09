7b10c78a39d5aa51d5f82704854cd35a057a9a49
MH-BOTZ <101972530+MH-BOTZ@users.noreply.github.com>
2022-04-09 13:35:06 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -53,7 +53,7 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**🗂️ Query:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded by @File_Search_Bot_V3**"
+        mo_tech_yt = f"**🗂️ Query:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 © @File_Search_Bot_V3**"
         files = await get_filter_results(query=search)
         if files:
             for file in files:
